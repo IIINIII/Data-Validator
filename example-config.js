6d1fcdd4a61aebdefab85d98ba88573ee7b8e2d1
IIINIII <ibrokhim.shokirov@gmail.com>
2018-10-08 15:22:32 +0300

    Ready for alpha release

---
+++ example-config.js
@@ -0,0 +1,40 @@
+var config = {
+    // A basic object configuration
+    'chat_data': {
+        type: 'object',
+        props: {
+            to: {type: 'string'},
+            from: {type: 'string'},
+            msg: {type: 'string'}
+        }
+    },
+
+    // A basic string configuration
+    'username': {
+        type: 'string'
+    },
+
+    // A basic number configuration
+    'age': {
+        type: 'number'
+    },
+
+    // A little bit more complex configuration
+    'user_data_complex': {
+        type: 'object',
+        props: {
+            name: {type: 'string'},
+            surname: {type: 'string'},
+            isActive: {type: 'boolean'},
+            contact_info: {
+                type: 'object',
+                props: {
+                    email: {type: 'string'},
+                    phone: {type: 'string'}
+                }
+            }
+        }
+    }
+};
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,125 @@
+var validator = {
+    // Configuration for data validation
+    config: null,
+
+    // Returns true if data is valid
+    'isValid': function (configName, data) {
+        if(!this.config) {
+            console.log(
+                'No configuration assigned!\n' +
+                'Please assign your configuration and try again!\n' +
+                'Example: DataValidator.config = myconfig;'
+            );
+        } else {
+            return this.validate(this.config[configName], data);
+        }
+    },
+
+    'validate': function (conf, data, name = 'data') {
+        var isValid = this.typeChecker[this.getTypeFunction(conf.type)](data);
+
+        if (!isValid) {
+            console.log(
+                '-----------Validator Error-----------' +
+                '\nObject Path: ' + name +
+                '\nObject: ' + data +
+                '\nExpected: ' + conf.type +
+                '\nGot: ' + this.getTypeName(data) +
+                '\n-------------------------------------'
+            );
+        } else if (conf.props) {
+            Object.keys(conf.props).forEach(
+                value => {
+                    isValid &= this.validate(conf.props[value], data[value], name + '.' + value);
+                }
+            );
+        }
+
+        return isValid;
+    },
+
+    // Returns function name for the type's validator
+    getTypeFunction: function (type) {
+        return 'is' + type.replace(/^\w/, function (chr) {
+            return chr.toUpperCase();
+        });
+    },
+
+    // Returns type of value
+    getTypeName: function (value) {
+        var type = '';
+        Object.keys(this.typeChecker).forEach(
+            typeFunc => {
+                if (this.typeChecker[typeFunc](value)) {
+                    type = typeFunc.toLowerCase().substr(2);
+                }
+            }
+        );
+        return type;
+    },
+
+    // List of all type controls
+    typeChecker: {
+        // Returns if a value is a string
+        isString: function (value) {
+            return typeof value === 'string' || value instanceof String;
+        },
+
+        // Returns if a value is really a number
+        isNumber: function (value) {
+            return typeof value === 'number' && isFinite(value);
+        },
+
+        // Returns if a value is an array
+        isArray: function (value) {
+            return value && typeof value === 'object' && value.constructor === Array;
+        },
+
+        // Returns if a value is a function
+        isFunction: function (value) {
+            return typeof value === 'function';
+        },
+
+        // Returns if a value is an object
+        isObject: function (value) {
+            return value && typeof value === 'object' && value.constructor === Object;
+        },
+
+        // Returns if a value is null
+        isNull: function (value) {
+            return value === null;
+        },
+
+        // Returns if a value is undefined
+        isUndefined: function (value) {
+            return typeof value === 'undefined';
+        },
+
+        // Returns if a value is a boolean
+        isBoolean: function (value) {
+            return typeof value === 'boolean';
+        },
+
+        // Returns if a value is a regexp
+        isRegExp: function (value) {
+            return value && typeof value === 'object' && value.constructor === RegExp;
+        },
+
+        // Returns if value is an error object
+        isError: function (value) {
+            return value instanceof Error && typeof value.message !== 'undefined';
+        },
+
+        // Returns if value is a date object
+        isDate: function (value) {
+            return value instanceof Date;
+        },
+
+        // Returns if a Symbol
+        isSymbol: function (value) {
+            return typeof value === 'symbol';
+        }
+    }
+};
+
+module.exports = validator;
